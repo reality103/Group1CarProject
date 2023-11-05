903910a92e5a95d1ba241aa18a23da5bc5b5b880
ChillBoomVn <147826099+ChillBoomVn@users.noreply.github.com>
2023-11-05 21:32:52 +0700

    Update CarManager.java

--- src/com/CarManager.java
+++ src/com/CarManager.java
@@ -1,34 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com;
+import util.MyUtil;
+import java.util.ArrayList;
 
-/**
- *
- * @author reality
- */
 public class CarManager {
-    
-    
     public static void main(String[] args) {
-        BrandList bl1 = new BrandList();
-        
-        
-//        bl1.addBrand();
-        bl1.loadFromFile("C:\\Users\\2002t\\OneDrive\\Desktop\\OOP\\SE1866_PRO192\\CarPrj\\src\\Brands.txt");
-        bl1.listBrand();
-        CarList cl1 = new CarList(bl1);
-        
-        cl1.loadFromFile("C:\\Users\\2002t\\OneDrive\\Desktop\\OOP\\SE1866_PRO192\\CarPrj\\src\\Cars.txt");
-        cl1.listCar();
-        
-        
-        
-        
+        ArrayList<String> ops = new ArrayList<>();
+        ops.add("List all brands");
+        ops.add("Add a new brand");
+        ops.add("Search a brand based on its ID");
+        ops.add("Update a brand");
+        ops.add("Save brands to the file, named brands.txt");
+        ops.add("List all cars in ascending order of brand names");
+        ops.add("List cars based on a part of an input brand name");
+        ops.add("Add a car");
+        ops.add("Remove a car based on its ID");
+        ops.add("Update a car based on its ID");
+        ops.add("Save cars to file, named cars.txt");
+        ops.add("Exit");
+
+        BrandList brandList = new BrandList();
+        brandList.loadFromFile("brands.txt");
+
+        CarList carList = new CarList(brandList);
+        carList.loadFromFile();
+
+        Menu<String> menu = new Menu<>();
+
+        int choice;
+        do {
+            choice = menu.int_getChoice(ops);
+
+            switch (choice) {
+                case 1:
+                    brandList.listBrand();
+                    break;
+                case 2:
+                    brandList.addBrand();
+                    break;
+                case 3:
+                   String brandID = MyUtil.getString("Enter the brand ID: ", "Invalid input.");
+                    brandList.searchID(brandID, 1);
+                    break;
+                case 4:
+                    brandList.updateBrand();
+                    break;
+                case 5:
+                    brandList.saveToFile("brands.txt");
+                    break;
+                case 6:
+                    carList.listCars();
+                    break;
+                case 7:
+                    carList.printBasedBrandName();
+                    break;
+                case 8:
+                    carList.addCar();
+                    break;
+                case 9:
+                    carList.removeCar();
+                    break;
+                case 10:
+                    carList.updateCar();
+                    break;
+                case 11:
+                    carList.saveToFile();
+                    break;
+                case 12:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        } while (choice > 0 && choice < ops.size());
     }
-    
-            
-    
 }
